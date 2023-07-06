63fd46c15876fdf89ec01f2b31677d52039a3c82
Laisa Luz <132408773+laisaluz@users.noreply.github.com>
2023-07-06 18:46:13 -0300

    Update damas.py

--- damas.py
+++ damas.py
@@ -1,20 +1,25 @@
-# Função principal 
+# Função principal : responsável por executar o programa 
 
 import sys
 import offdamas
 import uudamas
 
 def Damas():
-    #print(sys.argv)
 
-  #2 e 1, pois a função principal também conta como argumento
+    ''' Função responsável por determinar o modo de jogo e executar o objeto "gerenciador" apropriado da classe "offdamas.GerenciadorJogo" ou "uudamas.GerenciadorJogo"'''
+
+   # Se o número de argumentos na linha de comando for 2, ou seja, o arquivo de texto e a função principal, então o programa instancia o objeto "geranciador" da classe "uudamas.GerenciadorJogo" e começa o modo offline
   
     if len(sys.argv) == 2:
-        gerenciador = offdamas.GerenciadorJogo()  #ativa o modo Offline 
+        gerenciador = offdamas.GerenciadorJogo()  
 
+    # Se for igual a 1 (apenas a função principal), então começará a versão Usuário vs Usuário 
+    
     elif len(sys.argv) == 1:
-        gerenciador = uudamas.GerenciadorJogo() #ativa o modo Usuários vs Usuário
+        gerenciador = uudamas.GerenciadorJogo() 
 
+    # Se nenhum dos casos acima for feito, então a entrada será inválida
+    
     else:
         print("Entrada invalida, é permitido até um arquivo")
     
