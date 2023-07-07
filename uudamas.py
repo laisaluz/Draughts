2a6a1467a610f8da0c71b0cf121b842f017d00b1
Laisa Luz <132408773+laisaluz@users.noreply.github.com>
2023-07-06 21:14:57 -0300

    Update uudamas.py

--- uudamas.py
+++ uudamas.py
@@ -1,3 +1,10 @@
+'''
+GRUPO :
+LAISA CAROLINE DA COSTA LUZ - 552531
+MARIA LUIZA FELIPE CAROLINO - 552655
+LUIZA ESTHER MARTINS PESSOA - 555516
+'''
+
 # Inicialmente, definimos que não existem capturas disponíveis para nenhum tipo de peça :
 
 possuiCapturasDisponiveis = {
